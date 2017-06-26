258c7eeaa4d3644969fdcd7728503fe415258505
Brian Barrow <briancbarrow@gmail.com>
2017-06-26 14:05:17 -0600

    Added Sass file, added Favicon, renamed Portfolio controller to follow convention

--- mvc-portfolio/Controllers/PortfolioController.cs
+++ mvc-portfolio/Controllers/PortfolioController.cs
@@ -6,11 +6,11 @@ using System.Web.Mvc;
 
 namespace mvc_portfolio.Controllers
 {
-    public class Portfolio : Controller
+    public class PortfolioController : Controller
     {
         public ActionResult About()
         {
-            return View(about);
+            return View();
         }
     }
 }
