d6ef2bfc29ea7c80bd4cec52eda1d34c3fd6611b
Brian Barrow <briancbarrow@gmail.com>
2017-06-26 13:29:27 -0600

    Add project files.

---
+++ mvc-portfolio/Controllers/Portfolio.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvc_portfolio.Controllers
+{
+    public class Portfolio : Controller
+    {
+        public ActionResult About()
+        {
+            return View(about);
+        }
+    }
+}
